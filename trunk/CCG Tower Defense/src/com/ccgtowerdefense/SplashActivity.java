ad1dc16ac2ddf4dd3a5a45416da9c5741504f27c
lingelbachmb1@gmail.com <lingelbachmb1@gmail.com@f591b051-998a-2cfd-1031-a3a62c08ca74>
2012-08-08 19:12:16 +0000

    Added splash screen

---
+++ trunk/CCG Tower Defense/src/com/ccgtowerdefense/SplashActivity.java
@@ -0,0 +1,62 @@
+package com.ccgtowerdefense;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+
+public class SplashActivity extends Activity{
+    protected int _splashTime = 5000;
+    private Thread mSplashThread;  
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Splash screen view
+        setContentView(R.layout.splash);
+        
+        final SplashActivity sPlashScreen = this;   
+        
+        // The thread to wait for splash screen events
+        mSplashThread =  new Thread(){
+            @Override
+            public void run(){
+                try {
+                    synchronized(this){
+                        // Wait given period of time or exit on touch
+                        wait(_splashTime);
+                    }
+                }
+                catch(InterruptedException ex){                    
+                }
+
+                finish();
+                
+                // Run next activity
+                Intent intent = new Intent();
+                intent.setClass(sPlashScreen, MainActivity.class);
+                startActivity(intent);                   
+            }
+        };
+        
+        mSplashThread.start();
+    }
+    
+    /**
+     * Processes splash screen touch events
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent evt)
+    {
+        if(evt.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            synchronized(mSplashThread){
+                mSplashThread.notifyAll();
+            }
+        }
+        return true;
+    }
+}
