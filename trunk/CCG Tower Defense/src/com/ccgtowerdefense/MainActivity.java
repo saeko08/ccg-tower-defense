7d494abdbb8e4a5fa6c0ce3887608f1d611d634b
lingelbachmb@gmail.com <lingelbachmb@gmail.com@f591b051-998a-2cfd-1031-a3a62c08ca74>
2012-08-09 02:34:32 +0000

    Added basic buttons

--- trunk/CCG Tower Defense/src/com/ccgtowerdefense/MainActivity.java
+++ trunk/CCG Tower Defense/src/com/ccgtowerdefense/MainActivity.java
@@ -2,6 +2,7 @@ package com.ccgtowerdefense;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity
 {
@@ -11,5 +12,25 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public void onFindButtonClick(View view)
+    {
+        
+    }
+    
+    public void onCreateButtonClick(View view)
+    {
+        
+    }
+    
+    public void onDeckButtonClick(View view)
+    {
+        
+    }
+    
+    public void onCardsButtonClick(View view)
+    {
+        
     }
 }
